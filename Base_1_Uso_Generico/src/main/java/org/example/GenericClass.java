5fb86e9b7975d318e2126907642ab6a85d5976a9
Nelson Ortiz <nmortizf@unal.edu.co>
2026-05-03 16:56:15 -0500

    Trabajando con metodos genericos

---
+++ Base_1_Uso_Generico/src/main/java/org/example/GenericClass.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class GenericClass {
+
+    public <T> void myGenericMethod(T item){
+        System.out.println("El valor del item es " +  item.toString());
+    }
+
+    public <T, V> void multipleParametersGeneric(T item1, V item2){
+        System.out.println("El valor del item 1 es " + item1.toString());
+        System.out.println("El valor del item 2 es " + item2.toString());
+    }
+
+    public <T> T returnGeneric(T item){
+        System.out.println("El valor del item a retonar es " + item.toString());
+        return item;
+    }
+
+}
+
--- Base_1_Uso_Generico/src/main/java/org/example/Main.java
+++ Base_1_Uso_Generico/src/main/java/org/example/Main.java
@@ -20,6 +20,25 @@ public class Main {
         System.out.println();
         System.out.println("El valor String del objeto creado es:");
         System.out.println(myHashTable);
+        System.out.println();
+
+        // usando metodos genericos
+        GenericClass genericClass = new GenericClass();
+        genericClass.myGenericMethod(12.4);
+        genericClass.myGenericMethod("Una cadena de prueba");
+        genericClass.myGenericMethod(76.4F);
+        System.out.println();
+        // pasando multiples parametros
+        genericClass.multipleParametersGeneric("Nombre", "Nelson");
+        System.out.println();
+        genericClass.multipleParametersGeneric("Edad", 37);
+        System.out.println();
+        genericClass.multipleParametersGeneric("Casado", false);
+        System.out.println();
+        // devolviendo parametro Generico
+        genericClass.returnGeneric("Lucas");
+        System.out.println();
+        genericClass.returnGeneric(25.9);
 
     }
 }
